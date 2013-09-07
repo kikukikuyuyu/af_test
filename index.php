ad439e1c1aab52b2323b03eac3efc975b86d62fe
yusuke kikuchi <kikuchi@arrow-links.com>
2013-09-07 18:01:38 +0900

    typing

---
+++ index.php
@@ -0,0 +1,57 @@
+<html>
+	<head>
+		<title>JS タイピング</title>
+		
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="./com.css">
+		<!-- jquery1.10.1 -->
+		<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+		<script type="text/javascript" src="realPre1.01.js" ></script>
+	</head>
+	
+	<body>
+		<script type="text/javascript">
+			jQuery(document).ready(function(){
+			jQuery("#input_text").realPre();
+			});
+		</script>
+		
+		<div id = main_warpper>
+			
+			<div id = "content_header">
+				
+				<div class = "real">
+					
+				</div>
+				
+			</div>
+			
+			<div id = "content_body">
+				
+				<span class="guillemet_left"> < </span>
+				
+				<div id = "typing_tag">
+					
+					</php
+					
+					$text = "html";
+					echo $text;
+					
+					?>
+					
+				</div>
+				
+				<span class="guillemet_right"> > </span>
+				
+			</div>
+			
+			<div id = "content_footer">
+				
+				<input id = "input_text" type="text" name="user_tag" style="ime-mode:disabled" >
+				
+			</div>
+			
+			
+		</div>
+	</body>
+</html>
